86a4b884b81934c1560cdecab682c19b8165c662
Cédric Bonhomme <cedric@cedricbonhomme.org>
2022-02-15 08:19:23 +0100

    chg: [fetcher] use a timeout when retrieving data.

--- fit4cybersecuritystats/fetcher.py
+++ fit4cybersecuritystats/fetcher.py
@@ -4,13 +4,13 @@
 from urllib.parse import urljoin
 import requests
 
-
+TIMEOUT = 5
 DATE_FROM = ""
 DATE_TO = ""
 
 
 def survey_per_company_sector(instance):
     url = urljoin(instance[1], "/stats/survey_per_company_sector.json")
-    result = requests.get(url)
+    result = requests.get(url, timeout=TIMEOUT)
     assert result.status_code == 200, "error when getting data"
     return result.json()
