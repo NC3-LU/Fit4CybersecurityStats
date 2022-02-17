4e9c84f3ce8e24611ee2f5f3dbcf1eadb16b3efd
Cédric Bonhomme <cedric@cedricbonhomme.org>
2022-02-17 13:32:53 +0100

    chg: [html] include default Jinja template for Bokeh.

--- generate_html.py
+++ generate_html.py
@@ -12,6 +12,7 @@ from fit4cybersecuritystats.charts import survey_per_company_sector_chart
 
 env = Environment(loader=PackageLoader("fit4cybersecuritystats"))
 
+OUTPUT_PATH = "./stats-chart.html"
 
 if __name__ == "__main__":
     # Point of entry in execution mode
@@ -22,8 +23,23 @@ if __name__ == "__main__":
     for instance in INSTANCES:
         stats_data = survey_per_company_sector(instance)
         plots.append(survey_per_company_sector_chart(stats_data))
-    html = file_html(plots, (js_resources, css_resources), "Fit4CyberSecurity")
+
+    # html = file_html(plots, (js_resources, css_resources), "Fit4CyberSecurity")
+    renderJS = True
+    template = env.get_template("html/index.html")
+    html = file_html(
+        plots,
+        (js_resources, css_resources),
+        template=template,
+        template_variables={
+            'js_data': js_resources.render_js() if renderJS else '',
+            'text_heading': "<h1>Fit4Cybersecurity</h1>",
+            'text_download': "",
+            'text_footing': "",
+        }
+    )
+
     # print(html)
-    with open("./stats-chart.html", 'w') as f:
+    with open(OUTPUT_PATH, 'w') as f:
         f.write(str(html))
-    print("HTML pages written.")
+    print("HTML page written in {}.".format(OUTPUT_PATH))
